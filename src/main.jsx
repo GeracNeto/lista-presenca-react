787a7a215a12e0f132facff7543bb1039d8ff46a
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-07-03 09:52:37 -0300

    Adicionado comentários

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
+// Responsável por inicializar a apicação
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import './styles/global.css'
+import './styles/global.css' // Arquivos .js ou .jsx não é necessário colcoar a extensão no import. Porém, .css precisa
 
-import {Home} from './pages/Home'
+import {Home} from './pages/Home' // Colocar entre {} para no arquivo que está sendo chaamndo, não precisar usar "export default home", e ja escrever direto "export function Home"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render( // Acessa o id root e insere tudo o que está dentro do componente App
   <React.StrictMode>
     <Home />
   </React.StrictMode>
